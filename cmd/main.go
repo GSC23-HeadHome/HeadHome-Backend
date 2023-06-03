896794359bc51ce13aa1cefa9ca7feff8a240828
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-06-03 15:45:07 +0800

    Fix docs

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,17 @@
-// Package main is the entry point for the backend server of HeadHome-Backend, 
+// Package main is the entry point for the backend server of HeadHome-Backend,
 // a project focused on providing a robust and scalable solution to help alleviate the problem of dementia wandering.
 //
 // It initializes the server, configures the routes, and manages the database connection.
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/GSC23-HeadHome/HeadHome-Backend/database"
 	"github.com/GSC23-HeadHome/HeadHome-Backend/routes"
 )
 
-// main is the application entry point and manages API endpoints and routing. 
+// main is the application entry point and manages API endpoints and routing.
 func main(){
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
